1bd9ce3d278fa310d5c18fa37feb4fb300fcec11
Johannes <johannes.eifert@unifrch>
2013-03-20 17:44:17 +0100

    texture basics

--- SuperBall/SuperBall/Bomb.cpp
+++ SuperBall/SuperBall/Bomb.cpp
@@ -24,7 +24,34 @@ public:
 		glVertex3f(x+width, y, 0);
 		glVertex3f(x,y+height, 0);
 
+		//texture
+		GLuint bombTexture;
+		glGenTextures(1, &bombTexture);
+		glBindTexture(GL_TEXTURE_2D, bombTexture);
+		
+		// select modulate to mix texture with color for shading
+		glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
 
+		// when texture area is small, bilinear filter the closest mipmap
+		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                 GL_LINEAR_MIPMAP_NEAREST );
+		// when texture area is large, bilinear filter the original
+		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+		// the texture wraps over at the edges (repeat)
+		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+
+		BYTE * data;
+		FILE * file;
+		int imgwidth = 256, imgheight = 256; //to change with an actual image
+
+		// allocate buffer
+		data = malloc( width * height * 3 );
+		// open and read texture data
+		file = fopen( filename, "rb" );
+		fread( data, width * height * 3, 1, file );
+		fclose( file );
 
 		glEnd();
 
