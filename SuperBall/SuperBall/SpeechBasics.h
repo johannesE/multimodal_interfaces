a9ee156da114a6311df76b1e2ce059d2ce9562e2
Oleg Telegin <teleginoleg@yahoo.com>
2013-05-02 23:42:24 -1200

    this is the commit message

--- SuperBall/SuperBall/SpeechBasics.h
+++ SuperBall/SuperBall/SpeechBasics.h
@@ -1,3 +1,15 @@
+//------------------------------------------------------------------------------
+// <copyright file="SpeechBasics.h" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include "SuperBall.h"
+#include "KinectAudioStream.h"
+#include "resource.h"
+
 // For configuring DMO properties
 #include <wmcodecdsp.h>
 
@@ -15,14 +27,133 @@
 #include <sapi.h>
 #include <sphelper.h>
 
-
-
-
-#pragma once
-class SpeechBasics
+/// <summary>
+/// Main application class for SpeechBasics sample.
+/// </summary>
+class CSpeechBasics
 {
 public:
-	SpeechBasics(void);
-	~SpeechBasics(void);
-};
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    CSpeechBasics();
 
+    /// <summary>
+    /// Destructor
+    /// </summary>
+    ~CSpeechBasics();
+
+    /// <summary>
+    /// Handles window messages, passes most to the class instance to handle
+    /// </summary>
+    /// <param name="hWnd">window message is for</param>
+    /// <param name="uMsg">message</param>
+    /// <param name="wParam">message data</param>
+    /// <param name="lParam">additional message data</param>
+    /// <returns>result of message processing</returns>
+    static LRESULT CALLBACK MessageRouter(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+    /// <summary>
+    /// Handle windows messages for a class instance
+    /// </summary>
+    /// <param name="hWnd">window message is for</param>
+    /// <param name="uMsg">message</param>
+    /// <param name="wParam">message data</param>
+    /// <param name="lParam">additional message data</param>
+    /// <returns>result of message processing</returns>
+    LRESULT CALLBACK        DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+    /// <summary>
+    /// Creates the main window and begins processing
+    /// </summary>
+    /// <param name="hInstance">handle to the application instance</param>
+    /// <param name="nCmdShow">whether to display minimized, maximized, or normally</param>
+    int                     Run(HINSTANCE hInstance, int nCmdShow);
+
+private:
+    static LPCWSTR          GrammarFileName;
+    
+    // Main application dialog window
+    HWND                    m_hWnd;
+
+  
+
+    // Object that controls moving turtle around and displaying it
+    TurtleController*       m_pTurtleController;
+
+    // Current Kinect sensor
+    INuiSensor*             m_pNuiSensor;
+
+    // Audio stream captured from Kinect.
+    KinectAudioStream*      m_pKinectAudioStream;
+
+    // Stream given to speech recognition engine
+    ISpStream*              m_pSpeechStream;
+
+    // Speech recognizer
+    ISpRecognizer*          m_pSpeechRecognizer;
+
+    // Speech recognizer context
+    ISpRecoContext*         m_pSpeechContext;
+
+    // Speech grammar
+    ISpRecoGrammar*         m_pSpeechGrammar;
+
+    // Event triggered when we detect speech recognition
+    HANDLE                  m_hSpeechEvent;
+
+    /// <summary>
+    /// Create the first connected Kinect found.
+    /// </summary>
+    /// <returns>S_OK on success, otherwise failure code.</returns>
+    HRESULT                 CreateFirstConnected();
+
+    /// <summary>
+    /// Initialize Kinect audio stream object.
+    /// </summary>
+    /// <returns>S_OK on success, otherwise failure code.</returns>
+    HRESULT                 InitializeAudioStream();
+
+    /// <summary>
+    /// Create speech recognizer that will read Kinect audio stream data.
+    /// </summary>
+    /// <returns>
+    /// <para>S_OK on success, otherwise failure code.</para>
+    /// </returns>
+    HRESULT                 CreateSpeechRecognizer();
+
+    /// <summary>
+    /// Load speech recognition grammar into recognizer.
+    /// </summary>
+    /// <returns>
+    /// <para>S_OK on success, otherwise failure code.</para>
+    /// </returns>
+    HRESULT                 LoadSpeechGrammar();
+
+    /// <summary>
+    /// Start recognizing speech asynchronously.
+    /// </summary>
+    /// <returns>
+    /// <para>S_OK on success, otherwise failure code.</para>
+    /// </returns>
+    HRESULT                 StartSpeechRecognition();
+
+    /// <summary>
+    /// Process recently triggered speech recognition events.
+    /// </summary>
+    void                    ProcessSpeech();
+
+    /// <summary>
+    /// Maps a specified speech semantic tag to the corresponding action to be performed on turtle.
+    /// </summary>
+    /// <returns>
+    /// Action that matches <paramref name="pszSpeechTag"/>, or TurtleActionNone if no matches were found.
+    /// </returns>
+    TurtleAction            MapSpeechTagToAction(LPCWSTR pszSpeechTag);
+
+    /// <summary>
+    /// Set the status bar message.
+    /// </summary>
+    /// <param name="szMessage">message to display.</param>
+    void                    SetStatusMessage(const WCHAR* szMessage);
+};
