ffe5c3a0bf8d63f78ee58354b68f4401a6ce52e0
Johannes <johannes.eifert@unifrch>
2013-05-08 14:45:50 +0200

    more kinect stuff

---
+++ SuperBall/SuperBall/SpeechControl.cpp
@@ -0,0 +1,14 @@
+#include "SpeechControl.h"
+#include "stdafx.h"
+#include "resource.h"
+
+LPCWSTR SpeechControl::GrammarFileName = L"SpeechBasics-D2D.grxml";
+
+SpeechControl::SpeechControl(void)
+{
+}
+
+
+SpeechControl::~SpeechControl(void)
+{
+}
