3b78b8a697b44cd3297b67ad163ed95bb2125c70
Johannes <johannes.eifert@unifrch>
2013-05-09 16:08:40 +0200

    kinect finished but not connected to the program

--- SuperBall/SuperBall/SpeechControl.cpp
+++ SuperBall/SuperBall/SpeechControl.cpp
@@ -164,3 +164,249 @@ BallAction SpeechControl::MapSpeechTagToAction(LPCWSTR pszSpeechTag)
 
     return action;
 }
+HRESULT SpeechControl::CreateFirstConnected()
+{
+    INuiSensor * pNuiSensor;
+    HRESULT hr;
+
+    int iSensorCount = 0;
+    hr = NuiGetSensorCount(&iSensorCount);
+    if (FAILED(hr))
+    {
+        return hr;
+    }
+
+    // Look at each Kinect sensor
+    for (int i = 0; i < iSensorCount; ++i)
+    {
+        // Create the sensor so we can check status, if we can't create it, move on to the next
+        hr = NuiCreateSensorByIndex(i, &pNuiSensor);
+        if (FAILED(hr))
+        {
+            continue;
+        }
+
+        // Get the status of the sensor, and if connected, then we can initialize it
+        hr = pNuiSensor->NuiStatus();
+        if (S_OK == hr)
+        {
+            m_pNuiSensor = pNuiSensor;
+            break;
+        }
+
+        // This sensor wasn't OK, so release it since we're not using it
+        pNuiSensor->Release();
+    }
+
+    if (NULL != m_pNuiSensor)
+    {
+        // Initialize the Kinect and specify that we'll be using audio signal
+        hr = m_pNuiSensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_AUDIO); 
+        if (FAILED(hr))
+        {
+            // Some other application is streaming from the same Kinect sensor
+            SafeRelease(m_pNuiSensor);
+        }
+    }
+
+    if (NULL == m_pNuiSensor || FAILED(hr))
+    {
+        SetStatusMessage(L"No ready Kinect found!");
+        return E_FAIL;
+    }
+
+    hr = InitializeAudioStream();
+    if (FAILED(hr))
+    {
+        SetStatusMessage(L"Could not initialize audio stream.");
+        return hr;
+    }
+
+    hr = CreateSpeechRecognizer();
+    if (FAILED(hr))
+    {
+        SetStatusMessage(L"Could not create speech recognizer. Please ensure that Microsoft Speech SDK and other sample requirements are installed.");
+        return hr;
+    }
+
+    hr = LoadSpeechGrammar();
+    if (FAILED(hr))
+    {
+        SetStatusMessage(L"Could not load speech grammar. Please ensure that grammar configuration file was properly deployed.");
+        return hr;
+    }
+
+    hr = StartSpeechRecognition();
+    if (FAILED(hr))
+    {
+        SetStatusMessage(L"Could not start recognizing speech.");
+        return hr;
+    }
+
+    return hr;
+}
+HRESULT SpeechControl::InitializeAudioStream()
+{
+    INuiAudioBeam*      pNuiAudioSource = NULL;
+    IMediaObject*       pDMO = NULL;
+    IPropertyStore*     pPropertyStore = NULL;
+    IStream*            pStream = NULL;
+
+    // Get the audio source
+    HRESULT hr = m_pNuiSensor->NuiGetAudioSource(&pNuiAudioSource);
+    if (SUCCEEDED(hr))
+    {
+        hr = pNuiAudioSource->QueryInterface(IID_IMediaObject, (void**)&pDMO);
+
+        if (SUCCEEDED(hr))
+        {
+            hr = pNuiAudioSource->QueryInterface(IID_IPropertyStore, (void**)&pPropertyStore);
+    
+            // Set AEC-MicArray DMO system mode. This must be set for the DMO to work properly.
+            // Possible values are:
+            //   SINGLE_CHANNEL_AEC = 0
+            //   OPTIBEAM_ARRAY_ONLY = 2
+            //   OPTIBEAM_ARRAY_AND_AEC = 4
+            //   SINGLE_CHANNEL_NSAGC = 5
+            PROPVARIANT pvSysMode;
+            PropVariantInit(&pvSysMode);
+            pvSysMode.vt = VT_I4;
+            pvSysMode.lVal = (LONG)(2); // Use OPTIBEAM_ARRAY_ONLY setting. Set OPTIBEAM_ARRAY_AND_AEC instead if you expect to have sound playing from speakers.
+            pPropertyStore->SetValue(MFPKEY_WMAAECMA_SYSTEM_MODE, pvSysMode);
+            PropVariantClear(&pvSysMode);
+
+            // Set DMO output format
+            WAVEFORMATEX wfxOut = {AudioFormat, AudioChannels, AudioSamplesPerSecond, AudioAverageBytesPerSecond, AudioBlockAlign, AudioBitsPerSample, 0};
+            DMO_MEDIA_TYPE mt = {0};
+            MoInitMediaType(&mt, sizeof(WAVEFORMATEX));
+    
+            mt.majortype = MEDIATYPE_Audio;
+            mt.subtype = MEDIASUBTYPE_PCM;
+            mt.lSampleSize = 0;
+            mt.bFixedSizeSamples = TRUE;
+            mt.bTemporalCompression = FALSE;
+            mt.formattype = FORMAT_WaveFormatEx;	
+            memcpy(mt.pbFormat, &wfxOut, sizeof(WAVEFORMATEX));
+    
+            hr = pDMO->SetOutputType(0, &mt, 0);
+
+            if (SUCCEEDED(hr))
+            {
+                m_pKinectAudioStream = new KinectAudioStream(pDMO);
+
+                hr = m_pKinectAudioStream->QueryInterface(IID_IStream, (void**)&pStream);
+
+                if (SUCCEEDED(hr))
+                {
+                    hr = CoCreateInstance(CLSID_SpStream, NULL, CLSCTX_INPROC_SERVER, __uuidof(ISpStream), (void**)&m_pSpeechStream);
+
+                    if (SUCCEEDED(hr))
+                    {
+                        hr = m_pSpeechStream->SetBaseStream(pStream, SPDFID_WaveFormatEx, &wfxOut);
+                    }
+                }
+            }
+
+            MoFreeMediaType(&mt);
+        }
+    }
+
+    SafeRelease(pStream);
+    SafeRelease(pPropertyStore);
+    SafeRelease(pDMO);
+    SafeRelease(pNuiAudioSource);
+
+    return hr;
+}
+
+/// <summary>
+/// Create speech recognizer that will read Kinect audio stream data.
+/// </summary>
+/// <returns>
+/// <para>S_OK on success, otherwise failure code.</para>
+/// </returns>
+HRESULT SpeechControl::CreateSpeechRecognizer()
+{
+    ISpObjectToken *pEngineToken = NULL;
+    
+    HRESULT hr = CoCreateInstance(CLSID_SpInprocRecognizer, NULL, CLSCTX_INPROC_SERVER, __uuidof(ISpRecognizer), (void**)&m_pSpeechRecognizer);
+
+    if (SUCCEEDED(hr))
+    {
+        m_pSpeechRecognizer->SetInput(m_pSpeechStream, FALSE);
+        hr = SpFindBestToken(SPCAT_RECOGNIZERS,L"Language=409;Kinect=True",NULL,&pEngineToken);
+
+        if (SUCCEEDED(hr))
+        {
+            m_pSpeechRecognizer->SetRecognizer(pEngineToken);
+            hr = m_pSpeechRecognizer->CreateRecoContext(&m_pSpeechContext);
+
+            // For long recognition sessions (a few hours or more), it may be beneficial to turn off adaptation of the acoustic model. 
+            // This will prevent recognition accuracy from degrading over time.
+            //if (SUCCEEDED(hr))
+            //{
+            //    hr = m_pSpeechRecognizer->SetPropertyNum(L"AdaptationOn", 0);                
+            //}
+        }
+    }
+
+    SafeRelease(pEngineToken);
+
+    return hr;
+}
+
+/// <summary>
+/// Load speech recognition grammar into recognizer.
+/// </summary>
+/// <returns>
+/// <para>S_OK on success, otherwise failure code.</para>
+/// </returns>
+HRESULT SpeechControl::LoadSpeechGrammar()
+{
+    HRESULT hr = m_pSpeechContext->CreateGrammar(1, &m_pSpeechGrammar);
+
+    if (SUCCEEDED(hr))
+    {
+        // Populate recognition grammar from file
+        hr = m_pSpeechGrammar->LoadCmdFromFile(GrammarFileName, SPLO_STATIC);
+    }
+
+    return hr;
+}
+
+/// <summary>
+/// Start recognizing speech asynchronously.
+/// </summary>
+/// <returns>
+/// <para>S_OK on success, otherwise failure code.</para>
+/// </returns>
+HRESULT SpeechControl::StartSpeechRecognition()
+{
+    HRESULT hr = m_pKinectAudioStream->StartCapture();
+
+    if (SUCCEEDED(hr))
+    {
+        // Specify that all top level rules in grammar are now active
+        m_pSpeechGrammar->SetRuleState(NULL, NULL, SPRS_ACTIVE);
+
+        // Specify that engine should always be reading audio
+        m_pSpeechRecognizer->SetRecoState(SPRST_ACTIVE_ALWAYS);
+
+        // Specify that we're only interested in receiving recognition events
+        m_pSpeechContext->SetInterest(SPFEI(SPEI_RECOGNITION), SPFEI(SPEI_RECOGNITION));
+
+        // Ensure that engine is recognizing speech and not in paused state
+        hr = m_pSpeechContext->Resume(0);
+        if (SUCCEEDED(hr))
+        {
+            m_hSpeechEvent = m_pSpeechContext->GetNotifyEventHandle();
+        }
+    }
+        
+    return hr;
+}
+
+void SpeechControl::SetStatusMessage(const WCHAR* szMessage)
+{
+    // SendDlgItemMessageW(m_hWnd, IDC_STATUS, WM_SETTEXT, 0, (LPARAM)szMessage);
+} 
