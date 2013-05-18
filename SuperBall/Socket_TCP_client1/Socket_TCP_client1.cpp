e26f3570be083715e85296940ffba4db65528c1c
Dan Klas <klasdan@gmail.com>
2013-05-18 18:11:11 +0200

    two sockets ver.2

--- SuperBall/Socket_TCP_client1/Socket_TCP_client1.cpp
+++ SuperBall/Socket_TCP_client1/Socket_TCP_client1.cpp
@@ -8,14 +8,29 @@
 #include <iostream>
 #include <cstdio>
 
+
+/*
+
+Message format:
+
+vx float\0
+
+vy float\0
+
+ra float\0
+
+*/
+
+
 int main(int argc, char **argv)
 {
     WSADATA WSAData;
     SOCKET sock;
     SOCKADDR_IN sin;
-    char buffer[255];
+    char buffer[32];
     WSAStartup(MAKEWORD(2,0), &WSAData);
     sock = socket(AF_INET, SOCK_STREAM, 0);
+
 
     sin.sin_addr.s_addr = inet_addr("127.0.0.1");
     sin.sin_family = AF_INET;
@@ -26,6 +41,56 @@ int main(int argc, char **argv)
     recv(sock, buffer, sizeof(buffer), 0);
 
 	printf("Data received: %s",&buffer);
+
+	buffer[0]==' ';
+	buffer[1]=='\0';
+	int retval;
+	do
+	{
+		retval = send(sock, "vx 1245.65\0", 11, 0);
+		retval = recv(sock, buffer, sizeof(buffer), 0);
+	} 
+	while(retval == SOCKET_ERROR || (unsigned)strlen(buffer)==0 || buffer[0]=='r');  //'r' for "retransmit"
+
+
+	//Sleep(1000);
+
+	
+	do
+	{
+		retval = send(sock, "vy 5689.0\0", 10, 0);
+		retval = recv(sock, buffer, sizeof(buffer), 0);
+	} 
+	while(retval == SOCKET_ERROR || (unsigned)strlen(buffer)==0 || buffer[0]=='r');  //'r' for "retransmit"
+
+	//Sleep(1000);
+
+	
+	do
+	{
+		retval = send(sock, "ra 34\0", 6, 0);
+		retval = recv(sock, buffer, sizeof(buffer), 0);
+	} 
+	while(retval == SOCKET_ERROR || (unsigned)strlen(buffer)==0 || buffer[0]=='r');  //'r' for "retransmit"
+
+	//Sleep(1000);
+
+	
+	do
+	{
+		retval = send(sock, "ra 5\0", 5, 0);
+		retval = recv(sock, buffer, sizeof(buffer), 0);
+	} 
+	while(retval == SOCKET_ERROR || (unsigned)strlen(buffer)==0 || buffer[0]=='r');  //'r' for "retransmit"
+
+
+	do
+	{
+		retval = send(sock, "Bye\0", 4, 0);
+		retval = recv(sock, buffer, sizeof(buffer), 0);
+	} 
+	while(retval == SOCKET_ERROR || (unsigned)strlen(buffer)==0 || buffer[0]=='r');  //'r' for "retransmit"
+
 	char* s;
 	scanf("%s", &s);
 
--- SuperBall/Socket_TCP_server/Socket_TCP_server.cpp
+++ SuperBall/Socket_TCP_server/Socket_TCP_server.cpp
@@ -1,17 +1,38 @@
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
+#include <cstdio>
+//#include "SuperBall.h";
+
+/*
+
+Message format:
+
+vx float\0
+
+vy float\0
+
+ra float\0
+
+*/
+
 
 
 
 int main(int argc, char **argv)
 {
+	//WSACleanup();
+
+
+
     WSADATA WSAData;
     SOCKET sock;
-    SOCKET csock;
+    SOCKET sockFrom;
     SOCKADDR_IN sin;
-    SOCKADDR_IN csin;
+    SOCKADDR_IN from;
     WSAStartup(MAKEWORD(2,0), &WSAData);
     sock = socket(AF_INET, SOCK_STREAM, 0);
+	char Buffer[32];
+	char subbuff[32];
 
 
     sin.sin_addr.s_addr = inet_addr("127.0.0.1");//INADDR_ANY;
@@ -22,16 +43,111 @@ int main(int argc, char **argv)
     bind(sock, (SOCKADDR *)&sin, sizeof(sin));
     listen(sock, 0);
 
+	
 
-    while(1)
+	int retval;
+	bool connected=false;
+	bool acceptNewConnections = true;
+
+    while(acceptNewConnections)
     {
-        int sinsize = sizeof(csin);
-        if((csock = accept(sock, (SOCKADDR *)&csin, &sinsize)) != INVALID_SOCKET)
+		connected=false;
+		printf("The server socket on the addres %d listens on port %d.\n", sin.sin_addr.s_addr,  sin.sin_port);
+        int sinsize = sizeof(from);
+        if((sockFrom = accept(sock, (SOCKADDR *)&from, &sinsize)) != INVALID_SOCKET)
         {
-            send(csock, "Hello world!\r\n", 14, 0);
-            closesocket(csock);
+			printf("Server: accepted connection from %d, port %d\n", inet_ntoa(from.sin_addr),  htons(from.sin_port)) ;
+            retval = send(sockFrom, "Hello world!\r\n", 14, 0);
+
+			//retval = send(msgsock, Buffer, sizeof(Buffer), 0)
+			if (retval == SOCKET_ERROR)
+			{
+					fprintf(stderr,"Server: send() failed: error %d\n", WSAGetLastError());
+					connected=false;
+			}
+			else connected=true;
+
+			int stringsLength;
+			float parameter;
+
+			while(connected)
+			{
+
+				retval = recv(sockFrom, Buffer, sizeof(Buffer), 0);
+				//-----------
+
+				if (retval == SOCKET_ERROR)
+				{
+					fprintf(stderr,"Server: recv() failed: error %d. Disconnecting.\n", WSAGetLastError());
+					closesocket(sockFrom);
+					connected=false;
+				}
+				else
+				{
+					printf("Server: message received from %s, port %d: %s\n", inet_ntoa(from.sin_addr), htons(from.sin_port), Buffer);
+				}
+
+				stringsLength = (unsigned)strlen(Buffer);
+				if(stringsLength < 4) 
+				{
+					printf("Server: received string is shorter that 4 chars, disconnecting.\n");
+					closesocket(sockFrom);
+					connected=false;
+					acceptNewConnections = false;
+				}
+
+				memcpy( subbuff, &Buffer[3], stringsLength );
+				subbuff[stringsLength] = '\0';
+
+				printf("	Server: string value of the parameter: %s\n",subbuff);
+
+				parameter = (float)(atof(subbuff));
+
+				switch (Buffer[0])
+				{
+					case 'v': 
+					{
+						if(Buffer[1]=='x') printf("Server: update for vx: %f\n",parameter); //SuperBall::update_vX(parameter);
+						
+						else if(Buffer[1]=='y') printf("Server: update for vy: %f\n",parameter);  //SuperBall::update_vY(parameter);
+						
+						break;
+					}
+					case 'r':
+					{
+						 printf("Server: update for the radius: %f\n",parameter); //SuperBall::updateRadius(parameter);
+						break;
+					}
+				}
+
+				retval = send(sockFrom, "Ack\0", 4, 0);
+				if (retval == SOCKET_ERROR)
+				{
+					fprintf(stderr,"Server: send() failed: error %d. Disconnecting.\n", WSAGetLastError());
+					closesocket(sockFrom);
+					connected=false;
+				}
+				else
+				{
+					printf("Server: Ack sent\n");
+				}
+			}
+            
         }
+
+
+		
     }
-    /* On devrait faire closesocket(sock); puis WSACleanup(); mais puisqu'on a entré une boucle infinie ... */
+	
+
+	closesocket(sock);
+	printf("closesocket()\n");
+	
+	WSACleanup();
+
+	printf("\nBye");
+
+	scanf("%s",&subbuff);
+
     return 0;
 }
